3c595cc9d0cf2bd158142339870f81f965ff0bbc
M-Davy <mmutukudavis@gmail.com>
2026-01-21 11:29:50 +0300

    first commit

---
+++ POS/src/test/java/Retail/POS/PosApplicationTests.java
@@ -0,0 +1,13 @@
+package Retail.POS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
